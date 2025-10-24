eb82ac43702d5bc04c0984af57ee4b2722656235
SASWATI PADHI <saswatipadhi779@gmail.com>
2025-10-25 01:26:52 +0530

    some utils code added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // require('dotenv').config({path: './env'}) // but isme require and baki sab me import which is not good code ki consistency ko kharab karta hai
 
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 import dotenv from 'dotenv'
 
@@ -9,26 +10,16 @@ dotenv.config({
 
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("ERRR:", error)
+        throw error
+    })
 
-// import mongoose from 'mongoose'
-// import { DB_NAME } from './constants'
-
-// import express from 'express'
-// const app = express()
-
-// ;( async () => {
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error", (error) => {
-//             console.log("ERRR:", error);
-//             throw error
-//         })
-
-//         app.listen(process.env.PORT, () => {
-//             console.log(`App is listening on port ${process.env.PORT}`)
-//         })
-//     } catch (error) {
-//         console.log("ERROR:", error)
-//         throw err
-//     }
-// })()
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server Started at port: ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("MONGODB Connection Failed", error)
+})
